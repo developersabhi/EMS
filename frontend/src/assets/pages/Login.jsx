6b7c03ee6dc9fbfecaff36f5ba14b8a4cd75c2da
Abhishek <chaturvediabhi111@gmail.com>
2026-06-15 12:38:03 +0530

    Frontend add the useState and create handleSubmit  functional

--- frontend/src/assets/pages/Login.jsx
+++ frontend/src/assets/pages/Login.jsx
@@ -1,6 +1,20 @@
-// import React from 'react'
+import axios from 'axios';
+import  { useState } from 'react'
 
 const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit =async (e) =>{
+        e.preventDefault()
+        try {
+            const response = await axios.post("http://localhost:3000/api/auth/login",{email, password}
+            );
+            console.log(response)
+        } catch (error) {
+            console.log(error)
+        }
+    }
   return (
     <div className="flex flex-col items-center h-screen justify-center bg-gradient-to-b from-teal-600 from-50% to-gray-100 to-50% space-y-6">
       <h2 className="font-pacific text-3xl text-white">
@@ -8,14 +22,16 @@ const Login = () => {
       </h2>
       <div className="border shadow p-6 w-80 bg-white">
         <h2 className="text-2xl font-bold mb-4">Login</h2>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label htmlFor="email" className="block text-gray-700">Email</label>
-            <input type="email" className="w-full px-3 py-2 border " placeholder="Enter Email" />
+            <input type="email" className="w-full px-3 py-2 border " placeholder="Enter Email" 
+            onChange={(e)=>setEmail(e.target.value)}/>
           </div>
           <div className="mb-4">
             <label htmlFor="password" className="block text-gray-700">Password</label>
-            <input type="password" className="w-full px-3 py-2 border" placeholder="Enter Password" />
+            <input type="password" className="w-full px-3 py-2 border" placeholder="Enter Password" 
+            onChange={(e)=>setPassword(e.target.value)}/>
           </div>
           <div className="mb-4 flex items-center justify-between">
             <label htmlFor="" className="inline-flex items-center">
